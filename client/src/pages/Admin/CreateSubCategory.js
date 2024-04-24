46b6609ece6cd011820268a51a07353ef9eaf797
DitjonZeq <165717163+DitjonZeq@users.noreply.github.com>
2024-04-24 10:29:14 +0000

    update , delete per subcat si dhe disa ndryshime

--- client/src/pages/Admin/CreateSubCategory.js
+++ client/src/pages/Admin/CreateSubCategory.js
@@ -3,7 +3,7 @@ import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
-
+import { Modal, Select } from "antd";
 
 import SubCategoryForm from "../../components/Form/SubCategoryForm";
 
@@ -14,7 +14,10 @@ const CreateSubCategory = () => {
   const [subCategories, setSubCategories] = useState([]);
   const [subname, setSubname] = useState("");
   const [category, setCategory] = useState("");
- 
+  const [visible, setVisible] = useState(false);
+  const [selectedSubCategory, setSelectedSubCategory] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -68,9 +71,50 @@ const CreateSubCategory = () => {
     getAllSubCategory();
   }, []);
 
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `/api/v1/subcategory/update-subcategory/${selectedSubCategory._id}`,
+        { 
+          subname: updatedName,
+          category: selectedCategory
+        }
+      );
+      if (data?.success) {
+        toast.success(`${updatedName} is updated`);
+        setSelectedSubCategory(null);
+        setUpdatedName("");
+        setSelectedCategory("");
+        setVisible(false);
+        getAllCategory();
+        getAllSubCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong while updating the subcategory");
+    }
+  };
 
-
-  
+  const handleDelete = async (subCategoryId) => {
+    try {
+      const { data } = await axios.delete(
+        `/api/v1/subcategory/delete-subcategory/${subCategoryId}`
+      );
+      if (data.success) {
+        toast.success(`Subcategory is deleted`);
+        getAllCategory();
+        getAllSubCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong while deleting the subcategory");
+    }
+  };
 
   return (
     <Layout title={"Dashboard - Create SubCategory"}>
@@ -115,42 +159,66 @@ const CreateSubCategory = () => {
                     <th scope="col">Actions</th>
                   </tr>
                 </thead>
-              
                 <tbody>
-  {subCategories.map((s) => (
-    <tr key={s._id}>
-     <td>{categories.find(c => c._id === s.category)?.name}</td>
-      <td>{s.subname}</td>
-      <td>
-        <button
-          className="btn btn-primary ms-2"
-          onClick={() => {
-            setVisible(true);
-            setUpdatedName(s.subname);
-            setSelectedCategory(s.category._id);
-            setSelectedSubCategory(s);
-          }}
-        >
-          Edit
-        </button>
-        <button
-          className="btn btn-danger ms-2"
-          onClick={() => {
-            handleDelete(s._id);
-          }}
-        >
-          Delete
-        </button>
-      </td>
-    </tr>
-  ))}
-</tbody>
-
+                  {subCategories.map((s) => (
+                    <tr key={s._id}>
+                      <td>{categories.find(c => c._id === s.category)?.name}</td>
+                      <td>{s.subname}</td>
+                      <td>
+                        <button
+                          className="btn btn-primary ms-2"
+                          onClick={() => {
+                            setVisible(true);
+                            setUpdatedName(s.subname);
+                            setSelectedCategory(s.category._id);
+                            setSelectedSubCategory(s);
+                          }}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="btn btn-danger ms-2"
+                          onClick={() => {
+                            handleDelete(s._id);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
               </table>
             </div>
-            
-
-
+            <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              visible={visible}
+            >
+              <div className="p-3">
+                <Select
+                  bordered={false}
+                  placeholder="Select a category"
+                  size="large"
+                  showSearch
+                  className="form-select mb-3"
+                  onChange={(value) => setSelectedCategory(value)}
+                  value={selectedCategory}
+                >
+                  {categories?.map((c) => (
+                    <Option key={c._id} value={c._id}>
+                      {c.name}
+                    </Option>
+                  ))}
+                </Select>
+                <SubCategoryForm
+                  value={updatedName}
+                  setValue={setUpdatedName}
+                  handleSubmit={handleUpdate}
+                  categories={categories}
+                />
+              </div>
+            </Modal>
           </div>
         </div>
       </div>
