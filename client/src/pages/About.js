d58607b9d02ebfd68aa2bc14df53d202e0164894
Ditjon_Zeqiraj <165717163+DitjonZeq@users.noreply.github.com>
2024-05-23 16:39:46 +0000

    Change in about, contact and privacy policy

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,23 +3,25 @@ import Layout from "./../components/Layout/Layout";
 
 const About = () => {
   return (
-    <Layout title={"About us - Ecommer app"}>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
+    <Layout title={"About Us - ECOMMERCE APP"}>
+      <div className="row contactus">
+        <div className="col-md-6">
           <img
             src="/images/about.jpeg"
-            alt="contactus"
+            alt="About Us"
             style={{ width: "100%" }}
           />
         </div>
-        <div className=" col-md-4">
+        <div className="col-md-4">
+          <h2>About Us</h2>
           <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+            Welcome to ECOMMERCE APP, your number one source for all things [Product Categories]. We're dedicated to providing you the very best of [Product Types], with an emphasis on quality, customer service, and uniqueness.
+          </p>
+          <p className="text-justify mt-2">
+            Founded in [Year] by [Founder's Name], Ecomerce has come a long way from its beginnings in [Starting Location]. When [Founder's Name] first started out, their passion for [Industry/Products] drove them to start their own business.
+          </p>
+          <p className="text-justify mt-2">
+            We now serve customers all over [Country/World], and are thrilled that we're able to turn our passion into our own website. We aim to offer our customers a variety of the latest [Products]. We’ve come a long way, so we know exactly which direction to take when supplying you with high quality yet budget-friendly products.
           </p>
         </div>
       </div>
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -13,13 +13,23 @@ const Policy = () => {
           />
         </div>
         <div className="col-md-4">
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
+          <h2>Privacy Policy</h2>
+          <p>
+            <strong>Introduction</strong><br/>
+            We value the trust you place in us and are committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and safeguard your personal information.
+          </p>
+          <p>
+            <strong>Information We Collect</strong><br/>
+            We may collect the following types of information:
+            <ul>
+              <li><strong>Personal Information:</strong> Name, email address, phone number, shipping address, billing information, and payment details.</li>
+              <li><strong>Account Information:</strong> Username, password, and other authentication details.</li>
+              <li><strong>Order Information:</strong> Details of the products you purchase, order history, and transaction information.</li>
+              <li><strong>Technical Information:</strong> IP address, browser type, device information, and browsing behavior on our website.</li>
+            </ul>
+          </p>
+          
+           
         </div>
       </div>
     </Layout>
